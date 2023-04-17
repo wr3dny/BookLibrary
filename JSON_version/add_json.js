21b587168016baecc8ad1b725221802168513cf1
Pawe Pawtel <pawel.pawtel@gmail.com>
2023-04-17 17:53:38 +0200

    17/04 on and on

--- JSON_version/add_json.js
+++ JSON_version/add_json.js
@@ -1,10 +1,10 @@
 document.addEventListener("DOMContentLoaded", function () {
     document.querySelector("button").onclick = function () {
-        addBook();
+        save();
     };
 });
 
-const fs = require("fs"); 
+require('fs');
 
 class Book{
     constructor(name, surname, title, format){
@@ -15,95 +15,32 @@ class Book{
     }
 }
 
-// fetchBooks = () => {
-//     try{
-//         let notesJSON = fs.readFileSync('books.json');
-//         return JSON.parse(notesJSON);
-//     } catch (e) {
-//         return [];
-//     }
-// }
-
-// saveBook = (book) => {
-//     fs.writeFileSync('books.json', JSON.stringify(book));
-// }
-
-// addBook = (name, surname, title, format) => {
-//     let books = fetchBooks();
-//     let book = new Book(name, surname, title, format);
-//     books.push(book);
-//     saveBook(books);    
-// }
-
-// let name = document.querySelector('#name').value;
-// let surname = document.querySelector('#surname').value;
-// let title = document.querySelector('#title').value;
-// let format = document.querySelector('#format').value;
-
-// addBook(name, surname, title, format);
-// alert('Book added!')
+save = () => {
 
 
-
-
-
-
-addBook = () => {
     let name = document.querySelector('#name').value;
     let surname = document.querySelector('#surname').value;
     let title = document.querySelector('#title').value;
     let format = document.querySelector('#format').value;
 
-    fetchBooks = () => {
-        try{
-            let notesJSON = fs.readFileSync('books.json');
-            return JSON.parse(notesJSON);
-        } catch (e) {
-            return dane = [];
-        }
-    };
 
-    let saveBook = (book) => {
-        fs.writeFileSync("BooksList.json", JSON.stringify(book)); 
+    let daneJSON = fs.writeFileSync("BooksList.json", JSON.stringify(book));
+
+    let dane = [];
+    if(daneJSON == null){
+        dane = [];
+    }
+    else{
+        dane = JSON.parse(daneJSON);
     }
 
-    addBook = (name, surname, title, format) => {
-        let booksSoFar = fetchNotes();
-        let bookToSave = {
-          name,
-          surname,
-          title,
-          format
-        };
-          booksSoFar.push(bookToSave);
-          saveBook(scoresSoFar);
-          return bookToSave;
-    }; 
+    let book = new Book(name, surname, title, format);
+    dane.push(book)
 
-    addBook(name, surname, title, format);
-
-
-
-
-    // let daneJSON = localStorage.getItem('BK');
-    // let dane = [];
-    // if(daneJSON == null){
-    //     dane = [];
-    // }
-    // else{
-    //     dane = JSON.parse(daneJSON);
-    // }
-
-    // let book = new Book(name, surname, title, format);
-    // dane.push(book)
-
-    // daneJSON = JSON.stringify(dane);
-    // localStorage.setItem('BK', daneJSON);
-
-    // saveBook = (book) => {
-    // fs.writeFileSync('books.json', JSON.stringify(book));
-    // }
+    daneJSON = JSON.stringify(dane);
+    fs.writeFileSync('books.json', JSON.stringify(book))
 
     alert('Book added!')
 }
+
 
