c2cec6a7a8394049da1266d1de87b4017f24bd3f
Pawe Pawtel <pawel.pawtel@gmail.com>
2023-04-19 08:59:15 +0200

    19/04 9.oo styling

--- edit.js
+++ edit.js
@@ -13,12 +13,37 @@ edit = (index) => {
     html2 += `<input type="text" id="name" placeholder="Name" value="${dane[index].name}">`;
     html2 += `<input type="text" id="surname" placeholder="Surname" value="${dane[index].surname}">`;
     html2 += `<input type="text" id="title" placeholder="Title" value="${dane[index].title}">`;
-    html2 += `<input type="text" id="format" placeholder="Format" value="${dane[index].format}">`;
+    html2 += `<input type="text" id="format"value="${dane[index].format}">`; /// poprawić 
 
     html2 += `<button onclick="save(${index})">Save</button>`;
     html2 += "</ul>"
 
     document.querySelector("div#toEdit").innerHTML = html2;
+
+    save = () => {
+        let name = document.querySelector('#name').value;
+        let surname = document.querySelector('#surname').value;
+        let title = document.querySelector('#title').value;
+        let format = document.querySelector('#format').value;
+    
+    
+        let daneJSON = localStorage.getItem('BK');
+        let dane = [];
+        if(daneJSON == null){
+            dane = [];
+        }
+        else{
+            dane = JSON.parse(daneJSON);
+        }
+    
+        let book = new Book(name, surname, title, format);
+        dane.push(book)
+    
+        daneJSON = JSON.stringify(dane);
+        localStorage.setItem('BK', daneJSON);
+    
+        alert('Book added!')
+    }
 
 
 
@@ -66,27 +91,4 @@ show = () => {
 
 }
 
-save = () => {
-    let name = document.querySelector('#name').value;
-    let surname = document.querySelector('#surname').value;
-    let title = document.querySelector('#title').value;
-    let format = document.querySelector('#format').value;
 
-
-    let daneJSON = localStorage.getItem('BK');
-    let dane = [];
-    if(daneJSON == null){
-        dane = [];
-    }
-    else{
-        dane = JSON.parse(daneJSON);
-    }
-
-    let book = new Book(name, surname, title, format);
-    dane.push(book)
-
-    daneJSON = JSON.stringify(dane);
-    localStorage.setItem('BK', daneJSON);
-
-    alert('Book added!')
-}
